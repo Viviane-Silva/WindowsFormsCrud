e88b080e3a0d7d073758e3d041444fe571d17027
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-05-16 20:36:07 -0300

    Adicionando pesquisa por nome na listagem

--- WindowsFormsCrud/Form1.cs
+++ WindowsFormsCrud/Form1.cs
@@ -14,8 +14,9 @@ namespace WindowsFormsCrud
     public partial class Form1 : Form
     {
         MySqlConnection Conexao;
-
+        //cria conexao com o banco de dados
         string con = ("server=localhost;user=root;password=;database=crud-ibid");
+
         public Form1()
         {
             InitializeComponent();
@@ -74,6 +75,16 @@ namespace WindowsFormsCrud
         {
             try
             {
+                Conexao = new MySqlConnection(con);
+
+                string busca = "%" + campoPesquisa.Text + "%";
+
+                string sql = "SELECT * FROM db_produto WHERE nome LIKE " +busca+ " OR quantidade "+ busca;
+
+                MySqlCommand comando = new MySqlCommand(sql, Conexao);
+
+                Conexao.Open();
+                comando.ExecuteReader();
 
             }
             catch(Exception ex)
