c28c9448c54e46345ead05afaf89667a4ffe1dd8
Viviane-Silva <viviane.leite.silva@gmail.com>
2022-05-17 18:37:04 -0300

    Refatorando query

--- WindowsFormsCrud/Form1.cs
+++ WindowsFormsCrud/Form1.cs
@@ -18,6 +18,10 @@ namespace WindowsFormsCrud
         //https://mysqlconnector.net/connection-options/
         string con = ("server=localhost;user=root;password=;database=crud-ibid");
 
+
+        private int ?id_produto = null;
+
+
         public Sistema()
         {
             InitializeComponent();
@@ -38,30 +42,67 @@ namespace WindowsFormsCrud
             carregar_listagem();
         }
 
+
+
+        //botão salvar
         private void botaoSalvar_Click(object sender, EventArgs e)
         {
             try
             {
-
+                
                 Conexao = new MySqlConnection(con);
-
-                string sql = "INSERT INTO db_produto(nome_produto,quantidade,preco) VALUES ('" + campoNomeProd + "' ,'" + campoQuantidade + "' ,'" + campoPreco + "')";
-
-                MySqlCommand comando = new MySqlCommand(sql, Conexao);
-
                 Conexao.Open();
-                comando.ExecuteNonQuery();
-                MessageBox.Show(" Inserido com SUCESSO! ");
-                campoNomeProd.Clear();
-                campoQuantidade.Clear();
-                campoPreco.Clear();
 
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = Conexao;
+
+                if(id_produto == null)
+                {
+                    //insere novo produto
+                    cmd.CommandText = "INSERT INTO db_produto (nomeproduto,quantidade,preco) VALUES (@nomeproduto,@quantidade,@preco)";
+                    //cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@nomeproduto", campoNomeProd.Text);
+                    cmd.Parameters.AddWithValue("@quantidade", campoQuantidade.Text);
+                    cmd.Parameters.AddWithValue("@preco", campoPreco.Text);
+ 
+                    
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show(" Inserido com SUCESSO! ");
+                    
+                    campoNomeProd.Clear();
+                    campoQuantidade.Clear();
+                    campoPreco.Clear();
+
+
+               }
+               else
+                {
+                    //atualiza produto 
+                    cmd.CommandText = "UPDATE db_produto SET nomeproduto=@nomeproduto, quantidade=@quantidade, preco=@preco WHERE id=@id";
+                    //cmd.Prepare();
+                    cmd.Parameters.AddWithValue("@nomeproduto", campoNomeProd.Text);
+                    cmd.Parameters.AddWithValue("@quantidade", campoQuantidade.Text);
+                    cmd.Parameters.AddWithValue("@preco", campoPreco.Text);
+                    cmd.Parameters.AddWithValue("@id", id_produto);
+
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show(" Inserido com SUCESSO! ");
+                   // carregar_listagem();
+
+                    campoNomeProd.Clear();
+                    campoQuantidade.Clear();
+                    campoPreco.Clear();
+
+                }
+
+                //limpar tela e atualizar listagem
+                campoListagem.Items.Clear();
+                carregar_listagem();
 
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Erro: " + ex.Message);
-
             }
             finally
             {
@@ -70,11 +111,25 @@ namespace WindowsFormsCrud
 
         }
 
+
+
+        //campo listagem, pega informações do item selecionado
         private void listView1_SelectedIndexChanged(object sender, EventArgs e)
         {
+            ListView.SelectedListViewItemCollection itens_selecionados = campoListagem.SelectedItems;
 
+            //loop para pegar os dados do item selecionado
+            foreach(ListViewItem itens in itens_selecionados)
+            {
+                id_produto = Convert.ToInt32(itens.SubItems[0].Text); 
+                campoNomeProd.Text = itens.SubItems[1].Text;
+                campoQuantidade.Text = itens.SubItems[2].Text;
+                campoPreco.Text = itens.SubItems[3].Text;
+            }
         }
 
+
+        //botão busca(pesquisar)
         private void botaoPesquisa_Click_1(object sender, EventArgs e)
         {
             try
@@ -83,13 +138,13 @@ namespace WindowsFormsCrud
 
                 string busca = "'%" + campoPesquisa.Text + "%'";
 
-                string sql = "SELECT * FROM db_produto WHERE nome_produto LIKE " + busca ;
+                string sql = "SELECT * FROM db_produto WHERE nomeproduto LIKE " + busca ;
 
                 MySqlCommand comando = new MySqlCommand(sql, Conexao);
                 Conexao.Open();
 
                 MySqlDataReader leitor = comando.ExecuteReader();
-                campoListagem.Items.Clear();
+               campoListagem.Items.Clear();
 
                 //loop para buscar os dados
                 while (leitor.Read())
@@ -117,6 +172,9 @@ namespace WindowsFormsCrud
             }
         }
 
+
+
+        //campo listagem, tela carregada com as informações da base
         private void carregar_listagem()
         {
             try
@@ -128,9 +186,9 @@ namespace WindowsFormsCrud
 
                 cmd.Connection = Conexao;
                 cmd.CommandText = "SELECT * FROM db_produto ORDER BY id DESC ";
-                cmd.Parameters.AddWithValue("@busca", "%" + campoPesquisa.Text + "%");
                 cmd.Prepare();
-     
+    
+
                 MySqlDataReader reader = cmd.ExecuteReader();
                 //campoListagem.Items.Clear();
 
@@ -143,12 +201,18 @@ namespace WindowsFormsCrud
                         reader.GetString(2),
                         reader.GetString(3),
                     };
+
+                    campoListagem.Items.Add(new ListViewItem(linha));
                 }
 
             }
-            catch
+            catch(MySqlException ex)
             {
-
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+            finally
+            {
+                Conexao.Close();
             }
         }
 
